e9aed7561acee3e5fd2d07495076397dde4e7b1b
Adrien Roches <adrien@roc-it.tech>
2023-09-11 14:59:36 +0200

    [sfstart] 04-twig : Add movie list controller + updated templates

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Model\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
-    public function index(): Response
+    #[Route(
+        path: '/movies',
+        name: 'app_movies_list',
+        methods: ['GET']
+    )]
+    public function list(): Response
     {
-        return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+        return $this->render('movie/list.html.twig', [
+            'movies' => MovieRepository::listAll(),
         ]);
     }
 }
