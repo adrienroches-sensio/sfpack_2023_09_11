3bfcd34c096facb630b7d633c7806db0e31c5f4c
Adrien Roches <adrien@roc-it.tech>
2023-09-11 14:51:43 +0200

    [sfstart] 04-twig : Generate MovieController

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+    #[Route('/movie', name: 'app_movie')]
+    public function index(): Response
+    {
+        return $this->render('movie/index.html.twig', [
+            'controller_name' => 'MovieController',
+        ]);
+    }
+}
