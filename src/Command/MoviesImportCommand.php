44aa4bed56467ddd16da2d6f2b7d4f59b70eab03
Adrien Roches <adrien@roc-it.tech>
2023-09-13 15:51:40 +0200

    [sfmaster] 02-console : Define how to use the command

--- src/Command/MoviesImportCommand.php
+++ src/Command/MoviesImportCommand.php
@@ -12,15 +12,32 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:movies:import',
-    description: 'Add a short description for your command',
+    description: 'Search and import one or more movies into the database.',
+    aliases: [
+        'omdb:movies:import',
+        'movies:import',
+    ]
 )]
 class MoviesImportCommand extends Command
 {
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('id-or-title', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'IMDB ID or title to search.')
+            ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Will not import into database. Only displays what would happen.')
+            ->setHelp(<<<'EOT'
+                The <info>%command.name%</info> import movies data from OMDB API to database :
+                
+                Using only titles
+                    <info>php %command.full_name% "title 1" "title 2"</info>
+                
+                Using only ids
+                    <info>php %command.full_name% "ttid1" "ttid2"</info>
+                
+                Or mixing both
+                    <info>php %command.full_name% "ttid1" "title 2"</info>
+                EOT
+            )
         ;
     }
 
