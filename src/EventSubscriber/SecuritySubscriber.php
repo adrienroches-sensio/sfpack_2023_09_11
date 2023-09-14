ff69bcd53d1438753202d3f6a4175aba4a50fbec
Adrien Roches <adrien@roc-it.tech>
2023-09-14 15:05:59 +0200

    [sfmaster] 04-events : Update subscriber

--- src/EventSubscriber/SecuritySubscriber.php
+++ src/EventSubscriber/SecuritySubscriber.php
@@ -2,20 +2,40 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Clock\ClockInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class SecuritySubscriber implements EventSubscriberInterface
 {
-    public function onLoginSuccessEvent(LoginSuccessEvent $event): void
-    {
-        // ...
+    public function __construct(
+        private readonly ClockInterface $clock,
+        private readonly EntityManagerInterface $entityManager,
+    ) {
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            LoginSuccessEvent::class => 'onLoginSuccessEvent',
+            LoginSuccessEvent::class => [
+                ['updateLastLoggedInAt', 0]
+            ],
         ];
+    }
+
+    public function updateLastLoggedInAt(LoginSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $user->setLastLoggedInAt($this->clock->now());
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
     }
 }
